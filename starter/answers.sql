8046ffc0311903bdc3521af2df98a01775543b67
1U25CS029-GOBIKA <gobikasp2005@gmail.com>
2026-08-20 12:51:52 +0530

    Create database and student table with fields

--- starter/answers.sql
+++ starter/answers.sql
@@ -1,1 +1,5 @@
-
+CREATE DATABASE gobikaa;
+use gobikaa;
+create table STUDENT(StudentID integer(5) primary key,StudentName VARCHAR(20) NOT NULL,DOB date not null unique,Gender VARCHAR(10) not null,DepartmentID integer(5) not null);
+alter table STUDENT add Email VARCHAR(30);
+alter table STUDENT add PhoneNumber integer(10);
